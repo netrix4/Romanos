40a3e6859025010979ead00ca8acd62bc22dad1e
Mario <netrix4@gmail.com>
2025-03-14 18:08:33 -0700

    Add support for 2 ways in convertion go/py

--- ConversionNumeros.go
+++ ConversionNumeros.go
@@ -3,16 +3,18 @@ package main
 import (
 	"fmt"
 	"strings"
+	"strconv"
 )
 
-var romanValues = map[rune]int{
-	'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000,
+var romanValues = map[string]int{
+	"I": 1, "V": 5, "X": 10, "L": 50,
+	"C": 100, "D": 500, "M": 1000,
 }
 
 func calculateValues(values string) []int {
 	var individualValues []int
 	for _, digit := range values {
-		individualValues = append(individualValues, romanValues[digit])
+		individualValues = append(individualValues, romanValues[string(digit)])
 	}
 	return individualValues
 }
@@ -26,11 +28,65 @@ func setSumAbst(arabicValues []int) []int {
 	return arabicValues
 }
 
+func convertUnits(digit int) string {
+	switch digit {
+	case 9:
+		return "IX"
+	case 4:
+		return "IV"
+	case 5, 6, 7, 8:
+		return "V" + strings.Repeat("I", digit-5)
+	default:
+		return strings.Repeat("I", digit)
+	}
+}
+
+func convertTens(digit int) string {
+	switch digit {
+	case 9:
+		return "XC"
+	case 4:
+		return "XL"
+	case 5, 6, 7, 8:
+		return "L" + strings.Repeat("X", digit-5)
+	default:
+		return strings.Repeat("X", digit)
+	}
+}
+
+func convertHundreds(digit int) string {
+	switch digit {
+	case 9:
+		return "CM"
+	case 4:
+		return "CD"
+	case 5, 6, 7, 8:
+		return "D" + strings.Repeat("C", digit-5)
+	default:
+		return strings.Repeat("C", digit)
+	}
+}
+
+func convertThousands(digit int) string {
+	return strings.Repeat("M", digit)
+}
+
+func convertArabicToRoman(number int) string {
+	thousands := number / 1000
+	hundreds := (number % 1000) / 100
+	tens := (number % 100) / 10
+	units := number % 10
+
+	return convertThousands(thousands) +
+		convertHundreds(hundreds) +
+		convertTens(tens) +
+		convertUnits(units)
+}
+
 func convertNumbers(romanInput string) int {
 	allValues := calculateValues(romanInput)
 	signedAllValues := setSumAbst(allValues)
 	totalSum := 0
-	fmt.Println("All values:", allValues)
 	for _, value := range signedAllValues {
 		totalSum += value
 	}
@@ -38,28 +94,23 @@ func convertNumbers(romanInput string) int {
 }
 
 func isValidRomanNumber(text string) bool {
-	if isSyntacticallyCorrect(text) && isGrammaticallyCorrect(text) {
-		fmt.Println("Is the number valid? true")
-		return true
-	}
-	fmt.Println("Is the number valid? false")
-	return false
+	return isSyntacticallyCorrect(text) && isGrammaticallyCorrect(text)
+}
+
+func isValidArabicNumber(numberStr string) bool {
+	_, err := strconv.Atoi(numberStr)
+	return err == nil
 }
 
 func isGrammaticallyCorrect(text string) bool {
 	threeDigitCounter := 0
-	for i, x := range text {
-		if i > 0 && rune(text[i-1]) == x {
+	for i := 0; i < len(text); i++ {
+		if i > 0 && text[i-1] == text[i] {
 			threeDigitCounter++
 		} else {
 			threeDigitCounter = 0
 		}
-		if i > 2 && rune(text[i-2]) < x && rune(text[i-1]) < x {
-			fmt.Println("No more than 3 consecutive identical digits as a subst")
-			return false
-		}
 		if threeDigitCounter == 3 {
-			fmt.Println("No more than 3 consecutive identical digits are valid")
 			return false
 		}
 	}
@@ -67,27 +118,50 @@ func isGrammaticallyCorrect(text string) bool {
 }
 
 func isSyntacticallyCorrect(text string) bool {
-	for _, x := range text {
-		if _, exists := romanValues[x]; !exists {
-			fmt.Println("Invalid letter found:", string(x))
+	for _, char := range text {
+		if _, exists := romanValues[string(char)]; !exists {
 			return false
 		}
 	}
 	return true
 }
 
-func main() {
-	var isValidNumber bool
-	var upperInput string
-
-	for !isValidNumber {
-		var inputText string
+func romanToArabic() {
+	var inputText string
+	for {
 		fmt.Print("Give a valid roman notation number: ")
 		fmt.Scanln(&inputText)
-		upperInput = strings.ToUpper(inputText)
-		fmt.Println("Input:", upperInput)
-		isValidNumber = isValidRomanNumber(upperInput)
+		inputText = strings.ToUpper(inputText)
+		if isValidRomanNumber(inputText) {
+			break
+		}
 	}
+	fmt.Printf("Your number is: %d\n", convertNumbers(inputText))
+}
 
-	fmt.Println(convertNumbers(upperInput))
+func arabicToRoman() {
+	var inputText string
+	for {
+		fmt.Print("Give a valid arabic notation number: ")
+		fmt.Scanln(&inputText)
+		if isValidArabicNumber(inputText) {
+			break
+		}
+	}
+	number, _ := strconv.Atoi(inputText)
+	fmt.Printf("Your number is: %s\n", convertArabicToRoman(number))
+}
+
+func main() {
+	var conversionOption string
+	fmt.Println("(1) Roman to Arabic: MCMXCVII -> 1997")
+	fmt.Println("(2) Arabic to Roman: 1997 -> MCMXCVII")
+	fmt.Print("Select an option: ")
+	fmt.Scanln(&conversionOption)
+
+	if conversionOption == "1" {
+		romanToArabic()
+	} else if conversionOption == "2" {
+		arabicToRoman()
+	}
 }
--- ConversionRomanos.py
+++ ConversionRomanos.py
@@ -1,4 +1,5 @@
-# It took me about 5 hours
+# It took me about 5 hours XL -> 40
+#
 romanValues = {
    "I":1,
    "V":5,
@@ -16,7 +17,6 @@ def calculate_values(values):
         individualValues.append(romanValues[digit])
     return individualValues
 
-
 def set_sum_abst(arabicValues):
     currentIndex = 0
     for value in arabicValues:
@@ -24,6 +24,93 @@ def set_sum_abst(arabicValues):
             arabicValues[currentIndex-1] = arabicValues[currentIndex-1] * -1
         currentIndex += 1
     return arabicValues
+
+def convert_units(strToConvert):
+    digit = int(strToConvert)
+    value = ""
+    
+    if digit < 4:
+        for i in range(digit):
+            value = value + "I"
+    if digit == 4:
+        value = 'IV'
+    if digit > 4:
+        value = 'V'
+        if digit > 5:
+            if digit == 9:
+                value = "IX"
+            else:
+                for x in range(digit-5):
+                  value = value + "I"
+
+    return value
+
+def convert_tens(strToConvert):
+    digit = int(strToConvert)
+    value = ""
+
+    if digit < 4:
+        for i in range(digit):
+            value += "X"
+    if digit == 4:
+        value = "XL"
+    if digit > 4:
+        value = 'L'
+        if digit > 5:
+            if digit == 9:
+                value = "XC"
+            else:
+                for x in range(digit-5):
+                  value = value + "X"
+    return value
+
+def convert_hundreds(strToConvert):
+    digit = int(strToConvert)
+    value = ""
+
+    if digit < 4:
+        for i in range(digit):
+            value += "C"
+    if digit == 4:
+        value = "CD"
+    if digit > 4:
+        value = 'D'
+        if digit > 5:
+            if digit == 9:
+                value = "CM"
+            else:
+                for x in range(digit-5):
+                  value = value + "C"
+    return value
+
+def convert_thousands(strToConvert):
+    digit = int(strToConvert)
+    value = ""
+
+    for x in range(digit):
+        value += value + "M"
+    return value
+
+def convert_arabig_to_roman(numberInText):
+    totalRomanNumber = ""
+    newNumber = ""
+
+    digitCounter = 0
+
+    for number in reversed(numberInText):
+        digitCounter +=1
+
+        if digitCounter == 1:
+            newNumber = convert_units(number)
+        if digitCounter == 2:
+            newNumber = convert_tens(number)
+        if digitCounter == 3:
+            newNumber = convert_hundreds(number)
+        if digitCounter == 4:
+            newNumber = convert_thousands(number)
+        totalRomanNumber = newNumber + totalRomanNumber
+
+    return totalRomanNumber
 
 def convert_numbers(romanInput):
     allValues = calculate_values(romanInput)
@@ -48,6 +135,13 @@ def is_valid_roman_number(text):
 
     return response
 
+def is_valid_arabig_number(numberStr):
+    try:
+      inputTextNumber = int(numberStr)
+      return True
+    except:
+      return False
+
 def is_grammatically_correct(text):
     threeDigitCounter = 0
     currentIndex = 0
@@ -59,9 +153,9 @@ def is_grammatically_correct(text):
         else:
             threeDigitCounter = 0
 
-        if currentIndex > 2 and (text[currentIndex-2] < x) and (text[currentIndex-1] < x) :
-            print('No more than 3 consecutive identical digits as a subst')
-            return False
+        # if currentIndex > 2 and (text[currentIndex-2] < x) and (text[currentIndex-1] < x) :
+        #     print('No more than 3 consecutive identical digits as a subst')
+        #     return False
         
         if threeDigitCounter == 3:
             print('No more than 3 consecutive identical digits are valid')
@@ -77,17 +171,34 @@ def is_syntactically_correct(text):
         return False
     return True
 
-
-if __name__ ==  '__main__':
+def roman_2_arabic():
     isValidNumber = False
-    upperInput = ''
-
     while isValidNumber == False:
 
         inputText = input('Give a valid roman notation number: ')
         upperInput = inputText.upper()
         print('Input: ',upperInput)
-
         isValidNumber = is_valid_roman_number(upperInput)
       
-    print(convert_numbers(upperInput))
+    result = convert_numbers(upperInput)
+    print(f'Your number is: {result}')
+
+#///Nuevo
+def arabic_2_roman():
+    isValidNumber = False
+
+    while isValidNumber == False:
+      inputTextNumber = input('Give a valid arabig notation number: ')
+      isValidNumber = is_valid_arabig_number(inputTextNumber)
+
+    result = convert_arabig_to_roman(inputTextNumber)
+    print(f'Your number is: {result}')
+
+if __name__ ==  '__main__':
+    upperInput = ''
+    convertionOption = input('(1) Roman to arabic: MCMXCVII -> 1997\n(2) Arabic to roman: 1997 -> MCMXCVII\nSelect an option: ')
+
+    if convertionOption == '1':
+      roman_2_arabic()
+    if convertionOption == '2':
+      arabic_2_roman()
